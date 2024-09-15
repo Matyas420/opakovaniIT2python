7bec23fdc76da3e35dd3d42229f619336412780c
Matyas420 <159123612+Matyas420@users.noreply.github.com>
2024-09-15 18:52:41 +0200

    main.py

--- main.py
+++ main.py
@@ -19,21 +19,21 @@ print(len(prijmeni))
 # Číslo 6. Uložení hodnotu 6 do proměnné
 cislo = 6 
 
-# Číslo 7. Cyklus se opakuje dokud není roven 5 do té doby se do proměné x bude ukládat hodnota 3
+# Číslo 7. Cyklus se opakuje dokud není roven 5 do té doby se do proměné "cislo" bude ukládat hodnota 3
 for i in range(1,6):
     cislo += 3
     
 # Číslo 8. Výsledná hodnota se po pěti cyklech vypíše
 print(cislo)
 
-# Číslo 9. Uživatel zadá věk a isdigit zkontroluje zda je věk číslice tak program vypíše "Děkuji"
+# Číslo 9. Uživatel zadá věk a isdigit zkontroluje zda je věk číslice potom program vypíše "Děkuji" a když ne tak else
 vek = input("Zadejte věk: ")
 if vek.isdigit():
     print("Děkuji")
 else:
     print("Zadej jen celočíselnou hodnotu.")
 
-# Číslo 10. Program vygeneruje náhodnou číslo z listu "cisla" a pak ji uloží do proměnné "hodnota}}"
+# Číslo 10. Program vygeneruje náhodné číslo z listu "cisla" a pak ji uloží do proměnné "hodnota"
 cisla = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 hodnota = random.choice(cisla)
 print(hodnota)
